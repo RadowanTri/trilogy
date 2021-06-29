e87a4866f9afaadfb16115f18928c25a20144dc3
Radowan Bhuiyan <radowan.bhuiyan.tri@gmail.com>
2021-06-29 14:38:45 +0600

    trilogy

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function($){
+  "use strict";
+//==============================
+// smooth scroll
+//==============================
+
+
+  $(window).on('scroll', function () {
+    var wscroll = $(this).scrollTop();
+    //fixed-nav
+    wscroll > 1 ? $("#header").addClass("fixed") : $("#header").removeClass("fixed");
+
+    // go top appear
+    wscroll > 700 ? $("#goTop").fadeIn() : $("#goTop").fadeOut();
+
+  });
+
+
+  $( document ).ready(function() {
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      dots:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          800:{
+            items:3
+        },
+    
+          1000:{
+              items:4
+          },
+          1500:{
+            items:5
+        }
+      }
+  })
+
+
+});
+
+
+
+// portfolio popup image
+$('.image-link').magnificPopup({type:'image'});
+}(jQuery));
